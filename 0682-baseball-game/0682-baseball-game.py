f4f368b53a26bff2f716b70f91ef9b7b1368f9d9
Chala Bekabil <chalabekabil@gmail.com>
2025-02-17 09:15:34 +0300

    Time: 0 ms (100%), Space: 12.6 MB (40.08%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -2,12 +2,11 @@ class Solution(object):
     def calPoints(self, operations):
         record = []
         for operation in operations:
-            if(operation == "+"):
-                if len(record) >= 2:
-                    record.append(record[-1] + record[-2])
-            elif(operation == "D"):
+            if(operation == "+" and len(record) >= 2):
+                record.append(record[-1] + record[-2])
+            elif(operation == "D" and len(record) >= 1):
                 record.append(2 * record[-1])
-            elif(operation == "C"):
+            elif(operation == "C" and len(record) >= 1):
                 record.pop()
             else:
                 record.append(int(operation))
