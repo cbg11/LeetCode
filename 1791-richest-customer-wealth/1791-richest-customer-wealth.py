7eecf8b537045d198919432d39a008cad8db8ab5
Chala Bekabil <chalabekabil@gmail.com>
2025-02-19 16:37:37 +0300

    Time: 0 ms (100%), Space: 12.4 MB (56.56%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        max = 0
+        for i in accounts:
+            if sum(i) > max:
+                max = sum(i)
+        return max
+
+        
